7019cf57451bf6a804c107aa88d14722343ca0ee
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2023-10-27 19:09:46 +0700

    bersihin commit

--- Challenge/CH 4/myApp/public/scripts/app.example.js
+++ Challenge/CH 4/myApp/public/scripts/app.example.js
@@ -1,6 +1,6 @@
 class App {
   constructor() {
-    this.offCanvass = document.getElementById("offCanvass")
+    this.offCanvass = document.getElementById("offCanvass");
     this.date = document.getElementById("choose-date");
     this.time = document.getElementById("choose-time");
     this.passenger = document.getElementById("passenger");
@@ -22,24 +22,36 @@ class App {
     const filter = {
       dateTime: new Date(`${this.date.value}T${this.time.value}`),
       passenger: this.passenger.value,
-      typeDriver: this.typeDriver.value
-    }
+      typeDriver: this.typeDriver.value,
+    };
 
-    if (this.date == "" || this.time=="" || this.passenger=="" || this.typeDriver=="") {
+    if (
+      this.date == "" ||
+      this.time == "" ||
+      this.passenger == "" ||
+      this.typeDriver == ""
+    ) {
       alert("Please fill all fields");
     } else {
-    Car.list.forEach((car) => {
-      const timeCarJSON = new Date(car.availableAt).getTime();
-      if (timeCarJSON >=filter.dateTime.getTime() && car.available == true && car.capacity >= filter.passenger) {
-        const node = document.createElement("div");
-        node.classList.add("d-flex", "flex-lg-column", "card-car-result", "row-gap-lg-2")
-        node.innerHTML = car.render();
-        this.carContainerElement.appendChild(node);
-      }
-    });
+      Car.list.forEach((car) => {
+        const timeCarJSON = new Date(car.availableAt).getTime();
+        if (
+          timeCarJSON >= filter.dateTime.getTime() &&
+          car.available == true &&
+          car.capacity >= filter.passenger
+        ) {
+          const node = document.createElement("div");
+          node.classList.add(
+            "d-flex",
+            "flex-lg-column",
+            "card-car-result",
+            "row-gap-lg-2"
+          );
+          node.innerHTML = car.render();
+          this.carContainerElement.appendChild(node);
+        }
+      });
     }
-      
-    
   };
 
   async load() {
