87116c6789693aeec6f19b71e27b13552df04fc3
JuruLOSS <32490299+Ceiera@users.noreply.github.com>
2023-12-08 16:48:16 +0700

    somthng

--- Challenge/CH7/src/main.tsx
+++ Challenge/CH7/src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Router from "./routes/route";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router />
+    </React.StrictMode>
+  </Provider>
 );
